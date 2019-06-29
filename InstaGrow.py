e58a497e61e9e49885d255a50123d3f92cc5ccb0
Hamza Hassan <hassanhamza930@gmail.com>
2019-06-29 18:26:44 +0500

    Update InstaGrow.py

--- InstaGrow.py
+++ InstaGrow.py
@@ -1,3 +1,4 @@
+
 import time
 import keyboard  
 import random
@@ -17,14 +18,6 @@ options.add_argument('user-data-dir= selenium')
 browser = webdriver.Chrome(chrome_options=options, executable_path=r'chromedriver.exe')
 
 
-def tryy(x):
-    relay=True
-    while relay==True:
-        try:
-            x.click()
-            relay=False
-        except Exception:
-            continue
 
 def findAndClickFollow():
    g = 0
@@ -44,169 +37,85 @@ def findAndClickFollow():
    except Exception:
        g += 1
 
-def findAndClickFirstPicture():
-    anchors = browser.find_elements_by_tag_name('a')
-    g = 0
-    while g < len(anchors):
-        try:
-            href = anchors[g].get_attribute('href')
-            if '/p/' in href:
-                anchors[g].click()
-                g = len(anchors)
-            else:
+def FindAndClickFirstPicture():
+        anchors = browser.find_elements_by_tag_name('a')
+        g = 0
+        while g < len(anchors):
+            try:
+                href = anchors[g].get_attribute('href')
+                if '/p/' in href:
+                    anchors[g].click()
+                    g = len(anchors)
+                else:
+                    g += 1
+            except Exception:
                 g += 1
-        except Exception:
-            g += 1
 
-
-def findAndClickLikeButton():
-    buttons=browser.find_elements_by_tag_name('button')
-    for element in buttons:
-        print(element.text)
-
-def findAndClickUnfollow():
-   g = 0
-   try:
-    realbut = []
-    buttons = browser.find_elements_by_tag_name('button')
-    for element in buttons:
-        realbut.append(element.text)
-
-
-
-    while g < len(realbut):
-        if realbut[g] == 'Following':
-            buttons[g].click()
-            g = len(realbut)
-            time.sleep(2)
-        else:
-            g += 1
-   except Exception:
-       g += 1
-
-def findAndClickUnfollow2():
-   g = 0
-   try:
-    realbut = []
-    buttons = browser.find_elements_by_tag_name('button')
-    for element in buttons:
-        realbut.append(element.text)
-
-
-
-    while g < len(realbut):
-        if realbut[g] == 'Unfollow':
-            buttons[g].click()
-            g = len(realbut)
-            time.sleep(2)
-        else:
-            g += 1
-   except Exception:
-       g += 1
-
-
-def ClickAndIndexFollowing(ListToIndexTo):
-    browser.find_element_by_xpath('/html/body/span/section/main/div/header/section/ul/li[3]/a').click()
-    time.sleep(7)
-    a=browser.find_elements_by_tag_name('a')
-    hrefs=[]
-    for element in a:
-      try:
-        if element.get_attribute('title') not in hrefs:
-            hrefs.append((element.get_attribute('title')))
-      except Exception:
-          relay=True
-    hrefs.pop(0)
-    #print(hrefs)
-    ListToIndexTo=hrefs 
-    return ListToIndexTo
-    
-
-
-seed='meme____machine___'#input("Enter Seed          ")
-
-browser.get("https://www.instagram.com/"+seed)
-time.sleep(10)
-lista=[]
-FinalList=ClickAndIndexFollowing(lista)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-FinalList.pop(0)
-
-
-#print(FinalList)
-RealFinalList=[]
-
-for element in FinalList:
-    browser.get("https://www.instagram.com/"+element)
-    time.sleep(2)
-    relay=True
-    while relay== True:   
-        if keyboard.is_pressed('right'):  
-            relay=False
-        elif keyboard.is_pressed('down'):
-            RealFinalList.append("https://www.instagram.com/"+element)
-            relay=False
-print(RealFinalList)
-g=0
-for element in RealFinalList:
-    browser.get(element)
+def ClickFollowingAndIndex():
+  try: 
+    browser.find_element_by_xpath("/html/body/span/section/main/div/header/section/ul/li[3]/a").click()
     time.sleep(5)
-    try:
-        findAndClickFollow()
+    a=browser.find_elements_by_tag_name('a')
+
+    names=[]
+    for element in a:
+        title=element.get_attribute("title")
+        if title!="":
+            names.append(title)
+    return names
+  except Exception:
+      relay=True
+      
+def ClickLikeButton():
+  try:  
+    spans=browser.find_elements_by_tag_name("span")
+    for element in spans:
+        if element.get_attribute("aria-label")=="Like":
+            element.click()
+  except Exception:
+       relay=True
+
+#seed=input("Enter the seed account")
+browser.get("https://www.instagram.com/syed_zeerak_hussain_gillani")#("https://www.instagram.com/"+seed)
+time.sleep(10) 
+
+
+links=[]
+titles=ClickFollowingAndIndex()
+for element in titles:
+    links.append("https://www.instagram.com/"+element)
+
+print(links)
+
+f=0
+while f<100:
+    print(f)
+    browser.get(links[f])
+    relay=False
+    while relay==False:
+     if keyboard.is_pressed("down"):
+        relay=True
+        #findAndClickFollow()
+        FindAndClickFirstPicture()
         time.sleep(3)
-        findAndClickFirstPicture()
+        ClickLikeButton()
         time.sleep(3)
-        findAndClickLikeButton()
-        time.sleep(2)
-        browser.get(element)
+        browser.get(links[f])
         time.sleep(3)
-        ClickAndIndexFollowing(RealFinalList)
-        g+=1
-        time.sleep(randint.int(0,10))
-        if g>2:
-            break
+        temp=ClickFollowingAndIndex()
+        try:
+            for element in temp:
+                links.append("https://www.instagram.com/"+element)
+        except Exception:
+            continue
+        time.sleep(random.randint(5,11))
+        f+=1
+     if keyboard.is_pressed("right"):
+        relay=True
+        f+=1
 
-
-
-
-
-    except:
-        relay=True   
-        g+=1
-
-
-with open("array.txt","a") as file:
-    for element in RealFinalList:
-        file.write(element+'\n')
-
-        
-
-            
-        
-            
-             
+ 
+ 
 
     
-
-
-
 
